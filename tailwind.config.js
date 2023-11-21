41c1f9ad585417c649a39fcc5773982d09f5a1f0
luiz-matheus-da-silva <luizmatheus.profissional@hotmail.com>
2023-11-21 03:27:02 -0300

    Front-End

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3A3A3A",
+        secundary: "#939393",
+        third: '#503F2B',
+        bgfooter: '#D9D9D9',
+      }
+    },
+  },
+  plugins: [],
+}
